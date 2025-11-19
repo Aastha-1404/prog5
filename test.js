1d99d556153d7cbc112b1e41c484980199296951
Aastha-14 <aastha228767@gmail.com>
2025-11-20 00:33:56 +0530

    Add API tests for root endpoint

---
+++ test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("API Tests", () => {
+  it("Test root endpoint", (done) => {
+    request(app).get("/")
+      .expect(200, done);
+  });
+});
